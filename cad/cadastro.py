9fa215a46eb421f481a3b9d3a089e6f1213ca090
Anderson <anderson.bhilly@gmail.com>
2022-09-10 17:39:23 -0300

    finalizado

--- cad/cadastro.py
+++ cad/cadastro.py
@@ -3,11 +3,18 @@ from PySimpleGUI import PySimpleGUI as sg
 # Layout
 sg.theme('Reddit')
 layout =[
-    [sg.Text('Usuario'), sg.Input(key='usuario')],
-    [sg.Text('senha'), sg.Input(key='senha',password_char='*')],
+    [sg.Text('Usuario'), sg.Input(key='usuario', size = (20,1))],
+    [sg.Text('Senha'), sg.Input(key='senha',password_char='*', size = (20,1))],
     [sg.Checkbox('Salvar o login?')],
     [sg.Button('Enter')]
 ]
 # Janela
-
+janela = sg.Window('Tela de Login',layout)
 # Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WIN_CLOSED:
+        break
+    if eventos == 'Enter':
+        if valores['usuario'] == 'Anderson' and valores['senha'] == '2328':
+            print("Bem vindo!")
